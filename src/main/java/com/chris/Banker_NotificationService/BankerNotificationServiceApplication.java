2b446897b125eabf3144bc08f74fbdc56a657fcf
chris-mwesigwa <cmwesigwa@aetotechs.com>
2022-10-10 14:43:19 +0300

    initial commit

---
+++ src/main/java/com/chris/Banker_NotificationService/BankerNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_NotificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankerNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankerNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/Banker_NotificationService/BankerNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankerNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
